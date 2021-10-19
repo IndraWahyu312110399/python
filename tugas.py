48c2ef017ca33a73b927ffdde715b39fcfe79016
IndraWahyu312110399 <indrawahyu1456@gmail.com>
2021-10-19 16:15:33 +0700

    file tugas.py

---
+++ tugas.py
@@ -0,0 +1,32 @@
+#program input
+print(20*"_")
+
+
+Nama = str(input("nama = "))
+Nim = int(input("nim = "))
+
+print(" _________________ ")
+print(" \               / ")
+print("  \             /  ")
+print("   \           /   ")
+print("    \         /    ")
+print("     \       /     ")
+print("      \     /      ")
+print("       \   /       ")
+print("        \ /        ")
+print("         _         ")
+
+print("  ______________  ")
+print(" |              | ")
+print(" |  0        0  | ")
+print(" |              | ")
+print(" | _    --    _ | ")
+print(" |  \--------/  | ")
+print(" |   \      /   | ")
+print(" |    \____/    | ")
+print(" |______________| ")
+print("     -HAPPY-      ")
+
+print(20*"_")
+
+print("hai ",Nama," terimakasih sudah menginput nama")
